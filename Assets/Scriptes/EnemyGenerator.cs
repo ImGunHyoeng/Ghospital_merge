e5567cf48cd2d59c803719eb3141e080c735557b
PohangCandy <rlcks777@naver.com>
2023-05-17 21:18:34 +0900

    enemy error fix

--- Assets/Scriptes/EnemyGenerator.cs
+++ Assets/Scriptes/EnemyGenerator.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EnemyGenerator : MonoBehaviour
 {
@@ -9,8 +10,8 @@ public class EnemyGenerator : MonoBehaviour
     public GameObject enemyPrefab;
     int exist = 0; //enemy 이미 있으면 추가 생성 x
     float AllLightOff =  0; //모든 불이 꺼져있는지 체크
-    public int patten; 
-   
+    public int patten;
+    Scene scene;
 
     private void Awake()
     {
@@ -24,35 +25,22 @@ public class EnemyGenerator : MonoBehaviour
             Destroy(gameObject);
         }
     }
+    private void Start()
+    {
+       
+    }
 
 
     void Update()
     {
+        scene = SceneManager.GetActiveScene();
+        Debug.Log(scene.name);
         float check = 1.0f;
-        GameObject[] off = GameObject.FindGameObjectsWithTag("Light");
-        if (off == ) //복도에서 모든 방의 불이 다 꺼진 경우
+        
+        if (scene.name == "Play")//방안의 불이 꺼진 경우
         {
-            
-            int[] RoomLights = GameObject.Find("GameDirector").GetComponent<GameDirector>().RoomLights;
+            GameObject[] off = GameObject.FindGameObjectsWithTag("Light");
 
-            for (int i = 0; i < RoomLights.Length; i++)
-            {
-                check *= RoomLights[i];
-          
-            }
-            AllLightOff += check;
-            
-            if (this.exist == 0 && this.AllLightOff == 1.0f)
-            {
-               
-                this.enemy = Instantiate(enemyPrefab);
-                this.exist = 1;
-                this.patten = 2;
-            }
-        }
-        else //방안의 불이 꺼진 경우
-        {
-            Debug.Log("IM Here");
             for (int i = 0; i < off.Length; i++) // 모든 불이 꺼져있으면 AllLightoff =  1이 되도록 논리곱 사용
             {
                 check *= off[i].GetComponent<LightController>().lightOff;
@@ -67,6 +55,30 @@ public class EnemyGenerator : MonoBehaviour
             }
 
         }
+        if (scene.name == "Path")
+        {
+            //복도에서 모든 방의 불이 다 꺼진 경우
+            Debug.Log("IM Here");
+            int[] RoomLights = GameObject.Find("GameDirector").GetComponent<GameDirector>().RoomLights;
+
+            for (int i = 0; i < RoomLights.Length; i++)
+            {
+                check *= RoomLights[i];
+
+            }
+            AllLightOff += check;
+
+            if (this.exist == 0 && this.AllLightOff == 1.0f)
+            {
+
+                this.enemy = Instantiate(enemyPrefab);
+                this.exist = 1;
+                this.patten = 2;
+            }
+
+        }
+        
+        
         
     }
 }
