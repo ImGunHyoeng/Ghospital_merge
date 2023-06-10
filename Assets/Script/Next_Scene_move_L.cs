8e9e755d2bfffda8acb69f667f42352637bc1193
ImGunHyoeng <ynoe30@gmail.com>
2023-06-10 23:34:51 +0900

    fix:json reset fix

--- Assets/Script/Next_Scene_move_L.cs
+++ Assets/Script/Next_Scene_move_L.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -10,7 +11,8 @@ public class Next_Scene_move_L: MonoBehaviour
     // Start is called before the first frame update
     IEnumerator move_scene()
     {
-
+        if (DataManager.instance == null)
+            yield break;//코루틴 멈추는 함수
         DataManager.instance.playerData.movedirection = -1;
         DataManager.instance.SaveData();
 
@@ -19,7 +21,8 @@ public class Next_Scene_move_L: MonoBehaviour
         //yield return new WaitForSeconds(1);
         PlayerController.set_move_scene();
         Debug.Log(PlayerController.scene_move.ToString());
-        yield return new WaitForSeconds(0.1f);
+        //yield return new WaitForSeconds(0.1f);
+        yield return null;
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
--- Assets/Script/Player_Data.cs
+++ Assets/Script/Player_Data.cs
@@ -1,10 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
+using System;
+
+[Serializable]
 public class Player_Data 
 {
     //movedirection은 방향을 나타내는것 왼쪽으로 갔다면 -1,오른쪽으로 이동했다면 1
     public int movedirection;
-
+    public int extralife = 0;//추가 목숨
 }
